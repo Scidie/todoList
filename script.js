cdc75abbf5ce7b1e503719594e0b4f018cccf900
Scidie <patrykblaszczak1@gmail.com>
2019-04-07 13:57:18 +0200

    removed unnecessary functions and variables, scaled some parts of the code

--- script.js
+++ script.js
@@ -1,90 +1,14 @@
 /**
  * Main script file, currently handling all to-do list logic
  *
- * // pomyslalem zeby dodac jsdoc'i, zeby kod byl czytelniejszy, info wzialem z
- * // https://make.wordpress.org/core/handbook/best-practices/inline-documentation-standards/javascript/
- * // koment do wyjebania
- * 
  * @version 1.0
  * @author Scidie
  */
+
 var todoList = {
   todo: [],
-
-  addTodo: function(todoText) {
-    this.todo.push({
-      todoText: todoText,
-      completed: false
-    })
-  },
-
-  changeTodo: function(position, newValue) {
-    this.todo[position].todoText = newValue;
-  },
-
-  deleteTodo: function(position) {
-    this.todo.splice(position, 1);
-  },
-
-  toggleCompleted: function(position) {
-    var todo = this.todo[position - 1];
-    todo.completed = !todo.completed;
-  },
-
-  toggleAll: function() {
-    for (var i = 0; i < view.toggleList.length; i++) {
-
-      if (view.toggleList[i] === '( )') {
-        for (var i = 0; i < view.toggleList.length; i++) {
-            view.toggleList[i] = '(x)';
-        }
-      } else {
-        for (var i = 0; i < view.toggleList.length; i++) {
-          view.toggleList[i] = '( )';
-        }
-      }
-    }
-  }
-}
-
-var handlers = {
-  toggleAll: function() {
-    todoList.toggleAll();
-    view.displayTodo();
-  },
-
-  /**
-   * grabs input box reference, adds new element to list, ...
-   * 
-   * @author Scidie
-   */
-  addTodo: function() {
-    var addTodo = document.getElementById('add-todo-text-input');
-
-    if (addTodo.value != '') {
-      todoList.addTodo(addTodo.value);
-    }
-
-    addTodo.value = '';
-    view.addToggleListElement();
-    view.displayTodo();
-  },
-
-  changeTodo: function(position, text) {
-    todoList.changeTodo(position, text);
-    view.displayTodo();
-  },
-
-  deleteTodo: function(position) {
-    todoList.deleteTodo(position);
-    view.displayTodo();
-  }
-}
-
-// Viewing list object________________________________________________________________________________________________
-var view = {
   toggleList: [],
-  changeTodo: '',
+  todoId: '',
   addToggleListElement: function() {
     this.toggleList.push('( )') 
   },
@@ -93,6 +17,48 @@ var view = {
     this.toggleList.splice(position, 1);
   },
 
+  addTodo: function(todoText) {
+    var todoInputValue = document.getElementById('add-todo-text-input');
+    if (todoInputValue.value != '') {
+      console.log('im here');
+      this.todo.push({
+        todoText: todoInputValue.value,
+      })
+    }
+    todoInputValue.value = '';
+    this.addToggleListElement();
+    viewTodoList.displayTodo();
+  },
+
+  changeTodo: function(position, newValue) {
+    this.todo[position].todoText = newValue;
+    viewTodoList.displayTodo();
+  },
+
+  deleteTodo: function(position) {
+    this.todo.splice(position, 1);
+    viewTodoList.displayTodo();
+  },
+
+  toggleAll: function() {
+    for (var i = 0; i < todoList.toggleList.length; i++) {
+
+      if (todoList.toggleList[i] === '( )') {
+        for (var i = 0; i < todoList.toggleList.length; i++) {
+          todoList.toggleList[i] = '(x)';
+        }
+      } else {
+        for (var i = 0; i < todoList.toggleList.length; i++) {
+          todoList.toggleList[i] = '( )';
+        }
+      }
+    }
+    viewTodoList.displayTodo();
+  }
+}
+
+// Viewing todoList_______________________________________________________________________________________________________
+var viewTodoList = {
   displayTodo: function() {
     var theList = document.querySelector('ul');
     theList.innerHTML = '';
@@ -102,16 +68,20 @@ var view = {
       listElement.id = i;
       listElement.className = 'list-element';
       theList.appendChild(listElement);
-      listElement.appendChild(this.createToggleElement(i));
-      listElement.appendChild(this.createTextElement(i));
-      listElement.appendChild(this.createButtonElement());
+      listElement.appendChild(createHTMLObject.createToggleElement(i));
+      listElement.appendChild(createHTMLObject.createTextElement(i));
+      listElement.appendChild(createHTMLObject.createButtonElement());
     } 
-  },
+  }
+}
+
+//Functions creating HTML objects_________________________________________________________________________________________
+var createHTMLObject = {
 
   createToggleElement: function(position) {
     var toggleElement = document.createElement('li')
     toggleElement.className = 'toggle-element';
-    toggleElement.textContent = view.toggleList[position];
+    toggleElement.textContent = todoList.toggleList[position];
     return toggleElement;
   },
 
@@ -129,51 +99,45 @@ var view = {
     return buttonElement;
   }
 }
-
-// Main event listener_______________________________________________________________________________________________
+//Event Listners of created HTML objects__________________________________________________________________________________
 var ulList = document.querySelector('ul');
 
 ulList.addEventListener('click', function(event) {
-  console.log(event);
   var elementClicked = event.target;
-  var changeTextInput = document.getElementById('change-todo-text-input');
 
-  // sets "to-do edit field" to the value of the to-do that is being edited
-  changeTextInput.value = todoList.todo[parseInt(elementClicked.parentNode.id)].todoText;
+  var changeTodoTextInput = document.getElementById('change-todo-text-input');
+  changeTodoTextInput.value = todoList.todo[parseInt(elementClicked.parentNode.id)].todoText;
 
   if (elementClicked.className === 'delete-button') {
-    handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
-    view.deleteToggleElement(parseInt(elementClicked.parentNode.id));
+    todoList.deleteTodo(parseInt(elementClicked.parentNode.id));
+    todoList.deleteToggleElement(parseInt(elementClicked.parentNode.id));
   } 
   
   else if (elementClicked.className === 'toggle-element') {
-    console.log('klikam na element z id rownym: '+ elementClicked.parentNode.id);
-
       if (elementClicked.textContent === '( )') {
-        view.toggleList[parseInt(elementClicked.parentNode.id)] = '(x)';
+        todoList.toggleList[parseInt(elementClicked.parentNode.id)] = '(x)';
       }
       else {
-        view.toggleList[parseInt(elementClicked.parentNode.id)] = '( )';
+        todoList.toggleList[parseInt(elementClicked.parentNode.id)] = '( )';
       }
   } 
   
   else if (elementClicked.className === 'text-of-list-element') {
-    view.changeTodo = parseInt(elementClicked.parentNode.id);
+    todoList.todoId = parseInt(elementClicked.parentNode.id);
     console.log('jest tutaj');
     document.getElementById('third-line-buttons').style.visibility = 'visible';
   }
-  view.displayTodo();
+  viewTodoList.displayTodo();
 });
 
-var changeText = document.getElementById('change-todo-button');
-var changeTextInput = document.getElementById('change-todo-text-input');
+var changeTodoButton = document.getElementById('change-todo-button');
+var changeTodoTextInput = document.getElementById('change-todo-text-input');
 
-changeText.addEventListener('click', function(event) {
+changeTodoButton.addEventListener('click', function(event) {
   console.log(event);
-  var elementClicked = event.target;
-  handlers.changeTodo(view.changeTodo, changeTextInput.value);
+  todoList.changeTodo(todoList.todoId, changeTodoTextInput.value);
   document.getElementById('third-line-buttons').style.visibility = 'hidden';
-  changeTextInput.value = '';
+  changeTodoTextInput.value = '';
 });
 
 
