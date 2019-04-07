5f7cfecf3e596f58e8f9485dbb33d9a1c6ae3129
Scidie <patrykblaszczak1@gmail.com>
2019-04-07 20:17:07 +0200

    added switching machanism for 'change' and 'add todo' functions

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var todoList = {
   todo: [],
   toggleList: [],
   todoId: '',
+
   addToggleListElement: function() {
     this.toggleList.push('( )') 
   },
@@ -17,7 +18,7 @@ var todoList = {
     this.toggleList.splice(position, 1);
   },
 
-  addTodo: function(todoText) {
+  addTodo: function() {
     var todoInputValue = document.getElementById('add-todo-text-input');
     if (todoInputValue.value != '') {
       console.log('im here');
@@ -60,11 +61,11 @@ var todoList = {
 // Viewing todoList_______________________________________________________________________________________________________
 var viewTodoList = {
   displayTodo: function() {
-    var theList = document.querySelector('ul');
+    var theList = document.getElementById('list');
     theList.innerHTML = '';
 
     for (var i = 0; i < todoList.todo.length; i++) {
-      var listElement = document.createElement('li');
+      var listElement = document.createElement('div');
       listElement.id = i;
       listElement.className = 'list-element';
       theList.appendChild(listElement);
@@ -79,14 +80,14 @@ var viewTodoList = {
 var createHTMLObject = {
 
   createToggleElement: function(position) {
-    var toggleElement = document.createElement('li')
+    var toggleElement = document.createElement('span')
     toggleElement.className = 'toggle-element';
     toggleElement.textContent = todoList.toggleList[position];
     return toggleElement;
   },
 
   createTextElement: function(position) {
-    var textElement = document.createElement('li');
+    var textElement = document.createElement('span');
     textElement.className = 'text-of-list-element';
     textElement.textContent = todoList.todo[position].todoText;
     return textElement;
@@ -99,14 +100,22 @@ var createHTMLObject = {
     return buttonElement;
   }
 }
+
 //Event Listners of created HTML objects__________________________________________________________________________________
-var ulList = document.querySelector('ul');
+var ulList = document.getElementById('list');
+
+var changeTodoButton = document.getElementById('change-todo-button');
+var changeTodoTextInput = document.getElementById('change-todo-text-input');
+var addButton = document.getElementById('add-button');
+var todoTextInput = document.getElementById('add-todo-text-input');
+var buttonsContainer = document.getElementById('buttons-container');
+var firstLineButtons = document.getElementById('first-line-buttons');
+var secondLineButtons = document.getElementById('second-line-buttons');
 
 ulList.addEventListener('click', function(event) {
+  console.log(event);
   var elementClicked = event.target;
 
-  var changeTodoTextInput = document.getElementById('change-todo-text-input');
-  changeTodoTextInput.value = todoList.todo[parseInt(elementClicked.parentNode.id)].todoText;
 
   if (elementClicked.className === 'delete-button') {
     todoList.deleteTodo(parseInt(elementClicked.parentNode.id));
@@ -124,21 +133,34 @@ ulList.addEventListener('click', function(event) {
   
   else if (elementClicked.className === 'text-of-list-element') {
     todoList.todoId = parseInt(elementClicked.parentNode.id);
-    console.log('jest tutaj');
-    document.getElementById('third-line-buttons').style.visibility = 'visible';
+    buttonsContainer.replaceChild(secondLineButtons, firstLineButtons);
+    document.getElementById('second-line-buttons').style.visibility = 'visible';
+    changeTodoTextInput.value = todoList.todo[parseInt(elementClicked.parentNode.id)].todoText;
   }
   viewTodoList.displayTodo();
 });
 
-var changeTodoButton = document.getElementById('change-todo-button');
-var changeTodoTextInput = document.getElementById('change-todo-text-input');
 
 changeTodoButton.addEventListener('click', function(event) {
   console.log(event);
   todoList.changeTodo(todoList.todoId, changeTodoTextInput.value);
-  document.getElementById('third-line-buttons').style.visibility = 'hidden';
+  document.getElementById('second-line-buttons').style.visibility = 'hidden';
+  buttonsContainer.replaceChild(firstLineButtons, secondLineButtons);
   changeTodoTextInput.value = '';
 });
+
+addButton.addEventListener('click', function(event) {
+      todoList.addTodo();
+      todoTextInput.value = '';
+});
+
+
+
+
+
+
+
+
 
 
 
